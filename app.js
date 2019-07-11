9c7a2f38319b1b4a85576823a117d76cbc8b4558
SeyoungJeon <34870901+SeyoungJeon@users.noreply.github.com>
2019-07-11 10:13:30 +0900

    메뉴 디테일 화면 구성 및 mySQL 연결

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ var static = require('serve-static');
 
 var app = express();
 
+// MySql
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host : "localhost",
+  user : "root",
+  password : "123456",
+  insecureAuth : true
+});
+
+con.connect(function(err){
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
 app.use(static(path.join(__dirname,'/')));
 
-app.set('port'), process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8080);
 app.get('/', function(res,req){
   res.redirect('main.html');
 });
