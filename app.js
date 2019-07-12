9172ef9471db03f9048e2835d27a6882a47e04b9
LeeeeDain <dain6912@naver.com>
2019-07-12 19:36:21 +0900

    Modify cafeteria menu height

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ var static = require('serve-static');
 
 var app = express();
 
+
 app.use(static(path.join(__dirname,'/')));
 
 app.set('port', process.env.PORT || 8080);
+
 app.get('/', function(res,req){
   res.redirect('main.html');
 });
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Server Start...' + app.get('port'));
