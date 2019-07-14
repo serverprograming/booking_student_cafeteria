7b55b70ac1d39ee8619c323006a2790ff9408170
unknown <kcj409@naver.com>
2019-07-14 19:49:18 +0900

    세션 연동 및 예약 내역 관리자랑 회원 구분

--- sql_script/reserve_table.sql
+++ sql_script/reserve_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE reserve (
     id INT AUTO_INCREMENT,
     cafeteria VARCHAR(20) NOT NULL,
     menu VARCHAR(20) NOT NULL,
-    time VARCHAR(20),
+    time VARCHAR(30),
     complete VARCHAR(20),
 	user_id VARCHAR(20),
     price VARCHAR(20),
